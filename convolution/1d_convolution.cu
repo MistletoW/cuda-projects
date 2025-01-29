9f4150b30bad351b24041fdab9f3e9b02921cf4e
MistletoW <haasedylan@gmail.com>
2025-01-29 16:23:54 -0700

    convolve 1d finished

--- convolution/1d_convolution.cu
+++ convolution/1d_convolution.cu
@@ -1,2 +1,22 @@
 #include "1d_convolution.cuh"
 #include "tensor.cuh"
+
+using namespace std;
+
+int main(){
+
+}
+
+Tensor convolve1D(Tensor& input, Tensor& kernel){
+    if(input.getWidth() > kernel.getWidth()){
+        throw runtime_error("Input tensor width must be greater than kernel width.");
+    }
+
+    if(input.getHeight() != 1 || kernel.getHeight() != 1){
+        throw runtime_error("Height of both input and kernel tensors must be 1.");
+    }
+
+    if(input.getDepth() != 1 || kernel.getDepth() != 1){
+        throw runtime_error("Depth of input and kernel tensors must be 1.");
+    }
+}
